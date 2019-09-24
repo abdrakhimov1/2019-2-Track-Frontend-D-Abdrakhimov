47073d86b8c2315ab8d7b420863d7a4e39db391b
abdrakhimov1 <dan.abdrakhimov@yandex.ru>
2019-09-24 22:59:21 +0300

    First task done.

--- convertBytesToHuman.js
+++ convertBytesToHuman.js
@@ -6,6 +6,46 @@
  * и класса (например, отрицательные числа)
  */
 
-function convertBytesToHuman(bytes) {
-  // your solution goes here
+
+export default function convertBytesToHuman(bytes) {
+
+    if (typeof bytes !== "number") {
+        return "false";
+    }
+
+    if (bytes < 0) {
+        return "false";
+        /// bytes = bytes * -1
+    }
+
+    if (bytes < 1024) {
+        return bytes.toFixed(2).toString() + " Bytes";
+    }
+
+    bytes = bytes / 1024;
+
+    if (bytes < 1024) {
+        return bytes.toFixed(2).toString() + " KB";
+    }
+
+    bytes = bytes / 1024;
+
+    if (bytes < 1024) {
+        return bytes.toFixed(2).toString() + " MB";
+    }
+
+    bytes = bytes / 1024;
+
+    if (bytes < 1024) {
+        return bytes.toFixed(2).toString() + " GB"
+    }
+
+    bytes = bytes / 1024;
+
+    if (bytes < 1024) {
+        return bytes.toFixed(2).toString() + " TB"
+    }
+
+    return "false NUMB is too big.";
+
 }
--- tests/convertBytesToHuman.test.js
+++ tests/convertBytesToHuman.test.js
@@ -1,3 +1,5 @@
+import convertBytesToHuman from "../convertBytesToHuman.js"
+
 /*
  * Необходимо покрыть все возможные
  * и невозможные кейсы. Например,
@@ -9,13 +11,16 @@
 
 
 test('Возвращает false для неправильного типа данных', () => {
-  expect(convertBytesToHuman(/* ... */).toBe(/* ... */))
-  // ...
+    expect(convertBytesToHuman(-1)).toBe("false")
+    expect(convertBytesToHuman("asd")).toBe("false")
+        // ...
 });
 
 test('Возвращает корректное значение для чисел', () => {
-  expect(convertBytesToHuman(/* ... */).toBe(/* ... */))
-  // ...
+    expect(convertBytesToHuman(1024)).toBe("1.00 KB")
+    expect(convertBytesToHuman(123123123)).toBe("117.42 MB")
+
+    // ...
 });
 
 // другая группа проверок
