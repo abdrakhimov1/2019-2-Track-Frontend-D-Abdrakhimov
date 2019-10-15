2d64e27c05cf926f32376a5c0319cc42c1fb3944
Abdrakhimov Daniil <dan.abdrakhimov@yandex.ru>
2019-10-15 18:53:14 +0300

    Добавил сохранение сообщений в LocalStorage в формате json

--- web-components/src/components/MessageField.js
+++ web-components/src/components/MessageField.js
@@ -30,19 +30,59 @@ class MessageField extends HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.$field = this.shadowRoot.getElementById('field');
     this.$back = this.shadowRoot.getElementById('back');
+
+
+    if (!localStorage.messageNumber) {
+      localStorage.setItem('messageNumber', 0);
+    }
+
+    if (localStorage.messageNumber !== 0) {
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < localStorage.messageNumber; i++) { 
+        const workingJson = JSON.parse(localStorage[i]);
+
+        if (workingJson.user === 'Danny') {
+          const text = workingJson.message_text;
+          const time = workingJson.message_time;
+          const messageInformation = {
+            owner: workingJson.user,
+            text,
+            time };
+
+          this.createMessage(messageInformation);
+        }
+
+        if (workingJson.user === 'Other User') {
+          const text = workingJson.message_text;
+          const time = workingJson.message_time;
+          const messageInformation = {
+            owner: workingJson.user,
+            text,
+            time,
+          };
+          this.createMessage(messageInformation);
+        }
+      }
+    }
   }
 
   createMessage(messageInformation) {
     if (messageInformation.owner === 'me') {
       const message = document.createElement('new-my-message-form');
       message.setMessage(messageInformation);
+      // eslint-disable-next-line max-len
+      localStorage.setItem(localStorage.messageNumber, message.JsonMaker(localStorage.messageNumber));
       this.$field.appendChild(message);
+      localStorage.messageNumber = +(localStorage.messageNumber) + 1;
     }
 
     if (messageInformation.owner === 'not_me') {
       const message = document.createElement('new-friend-message-form');
       message.setMessage(messageInformation);
       this.$field.appendChild(message);
+      // eslint-disable-next-line max-len
+      localStorage.setItem(localStorage.messageNumber, message.JsonMaker(localStorage.messageNumber));
+      localStorage.messageNumber = +(localStorage.messageNumber) + 1;
     }
   }
 }
