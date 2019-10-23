6e126ac6a8ffdbe256865412b39082e9f567e9c7
abdrakhimov1 <dan.abdrakhimov@yandex.ru>
2019-10-23 12:02:22 +0300

    Task 3 and 4

--- web-components/src/components/Messanger.js
+++ web-components/src/components/Messanger.js
@@ -4,9 +4,7 @@ template.innerHTML = `
 
 
     #messanger {
-        display : flex;
-        flex-direction : row;
-        position: relative;
+
         }
     
     #friend-list{
@@ -15,17 +13,28 @@ template.innerHTML = `
     .message-form{
          
     }
-    
+
+    #list-of-messages{
+      display : flex;
+      flex-direction : column-reverse;
+    }
+
     </style>
 
     <form id="messanger">
         
-        <message-form ></message-form>
+        <friend-list></friend-list>
+        <div id='list-of-messages'>
+          <list-unit></list-unit>
+          <list-unit></list-unit>
+          <list-unit></list-unit>
+          <list-unit></list-unit>
+          <list-unit></list-unit>
+        </div>
+        <my-button></my-button>
         
     </form>
-    
 
-    
 `;
 
 class Messanger extends HTMLElement {
@@ -34,7 +43,15 @@ class Messanger extends HTMLElement {
     this.shadowRoot = this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.$message_field = this.shadowRoot.querySelector('message-form');
+    this.$listOfMessages = this.shadowRoot.getElementById('list-of-messages');
+    this.$superButton = this.shadowRoot.querySelector('my-button');
+    this.$superButton.onclick = this.AddChat;
+  }
+  AddChat() {
+    //  потом добавлю открытие меню для создания нового сообщения
+    this.$chat = document.createElement('list-unit');
+    this.$listOfMessages.appendChild(this.$chat);
+    return false;
   }
 }
-
 customElements.define('my-messanger', Messanger);
