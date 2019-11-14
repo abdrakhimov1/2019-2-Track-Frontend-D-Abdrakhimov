c1f06b609f20e69a80ca6188863abede812ba8e4
abdrakhimov1 <dan.abdrakhimov@yandex.ru>
2019-11-14 12:53:27 +0300

    components fix

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,75 +1,70 @@
-import React, {useState} from 'react';
-import MessageList from './MessageList';
-import './style_css/Compose.css';
-import Header from './Header';
-import styled from '@emotion/styled';
-import add from '../assets/document.png';
-import pic from '../assets/photo.png';
-import heart from '../assets/heart.png';
-
+import React, { useState } from 'react'
+import styled from '@emotion/styled'
+import MessageList from './MessageList'
+import './style_css/Compose.css'
+import Header from './Header'
+import add from '../assets/document.png'
+import pic from '../assets/photo.png'
+import heart from '../assets/heart.png'
 
 const ComposeForm = styled.div`
-    display: flex;
-    position: fixed;
-    bottom: 0px;
-    width: 100%;
-    height:55px;
-    background: #8b3d78;
-`;
+  display: flex;
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+  height: 55px;
+  background: #8b3d78;
+`
 
-export default function Chat(){
+export default function Chat() {
+  const [messageText, setMessageText] = useState('')
 
-	const [messageText, setMessageText] = useState('');
+  const [messages, setMessages] = useState([
+    { id: 1, text: 'Message Text1', UserName: 'User name', messageTime: 'date and time', wasRead: 'true' },
+    { id: 2, text: 'Message Text2', UserName: 'User name', messageTime: 'date and time', wasRead: 'true' },
+    { id: 3, text: 'Message Text3', UserName: 'User name', messageTime: 'date and time', wasRead: 'true' },
+  ])
 
+  const addMessage = (event) => {
+    if (event.key === 'Enter') {
+      if (messageText !== '') {
+        setMessages([
+          ...messages,
+          {
+            id: Date.now(),
+            UserName: 'Me',
+            messageTime: 'date and time',
+            wasRead: 'true',
+            text: messageText,
+          },
+        ])
+        setMessageText('')
+      }
+    }
+  }
 
-	const [messages, setMessages] = useState([
-		{id: 1,  text: 'Message Text1', UserName: 'User name', messageTime: 'date and time', wasRead: 'true'},
-		{id: 2, text: 'Message Text2', UserName: 'User name', messageTime: 'date and time', wasRead: 'true'},
-		{id: 3, text: 'Message Text3', UserName: 'User name', messageTime: 'date and time', wasRead: 'true'}  
-	]);
+  return (
+    <div>
+      <div className="container">
+        <Header />
+        <MessageList messages={messages} />
+      </div>
 
-	const addMessage = event => {
-		if (event.key === 'Enter'){
-			if (messageText !== ''){
-				setMessages([
-					...messages,
-					{
-						id: Date.now(),
-						UserName : 'Me',
-						messageTime: 'date and time',
-						wasRead: 'true',
-						text: messageText
-					}
-				]);
-				setMessageText('');
-			}
-		}
-	};
-
-
-	return (
-		<div>
-			<div className='container'>
-				<Header />
-				<MessageList messages={messages} />
-			</div>
-
-			<ComposeForm>
-				<img src={add} className="add" alt="add"/>
-				<input
-					type="text"
-					id = 'main-input'
-					className="compose-input"
-					placeholder="Введите сообщение"
-					autoComplete="off"
-					value = {messageText}
-					onChange={event => setMessageText(event.target.value)}
-					onKeyPress={addMessage}
-				/>
-				<img src={pic} className="add" alt="add"/>
-				<img src={heart} className="add" alt="add"/>
-			</ComposeForm>
-		</div>
-	);
-    
+      <ComposeForm>
+        <img src={add} className="add" alt="add" />
+        <input
+          type="text"
+          id="main-input"
+          className="compose-input"
+          placeholder="Введите сообщение"
+          autoComplete="off"
+          value={messageText}
+          onChange={(event) => setMessageText(event.target.value)}
+          onKeyPress={addMessage}
+        />
+        <img src={pic} className="add" alt="add" />
+        <img src={heart} className="add" alt="add" />
+      </ComposeForm>
+    </div>
+  )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from '@emotion/styled';
-import { keyframes } from '@emotion/core';
-import back from '../assets/back.png';
-import info from '../assets/info.png';
-import search from '../assets/search.png';
-import ava from '../assets/avatar.png';
-import PropTypes from 'prop-types';
-
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+import { keyframes } from '@emotion/core'
+import back from '../assets/back.png'
+import info from '../assets/info.png'
+import search from '../assets/search.png'
+import ava from '../assets/avatar.png'
 
 const rotate360 = keyframes`
 	from {
@@ -16,62 +15,62 @@ const rotate360 = keyframes`
 	to {
 		transform: rotate(360deg);
 	}
-`;
+`
 
 const TopBar = styled.div`
-	background-color: #550445;
-	display: inline-block;
-	justify-content: space-between;
-	align-items: center;
-	height: 60px;
-	padding: 20px;
-	color: #fff;
-	display: flex;
-	.redux-logo {
-		animation: ${rotate360} infinite 20s linear;
-		height: 80px;
-	}
-`;
+  background-color: #550445;
+  display: inline-block;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+  padding: 20px;
+  color: #fff;
+  display: flex;
+  .redux-logo {
+    animation: ${rotate360} infinite 20s linear;
+    height: 80px;
+  }
+`
 
 const UserNameForm = styled.div`
-	font-size: large;
-	margin-right: 15px;
-	font-weight: bold;
-	margin-left: 5px;
-	text-align: left;
-`;
+  font-size: large;
+  margin-right: 15px;
+  font-weight: bold;
+  margin-left: 5px;
+  text-align: left;
+`
 const LastSeenForm = styled.div`
-	font-weight: 200;
-	font-size: small;
-	margin-right: 5px;
-	text-align: left;
-	margin-left: 5px;
-`;
+  font-weight: 200;
+  font-size: small;
+  margin-right: 5px;
+  text-align: left;
+  margin-left: 5px;
+`
 const UserDisplayForm = styled.div`
-	display: flex;
-	flex-direction: column;
-`;
+  display: flex;
+  flex-direction: column;
+`
 
-function Header({UserName, lastSeen}) {
-	return (
-		<TopBar>
-			<Link to="/FriendList">
-				<img src={back} className="add" alt="add"/>
-			</Link>
-			<img src={ava} className="ava" alt="ava"/>
-			<UserDisplayForm>
-				<UserNameForm>UserName</UserNameForm>
-				<LastSeenForm>last:seen</LastSeenForm>
-			</UserDisplayForm>
-			<img src={info} className="add" alt="add"/>
-			<img src={search} className="add" alt="add"/>
-		</TopBar>
-	);
+function Header({ UserName, lastSeen }) {
+  return (
+    <TopBar>
+      <Link to="/FriendList">
+        <img src={back} className="add" alt="add" />
+      </Link>
+      <img src={ava} className="ava" alt="ava" />
+      <UserDisplayForm>
+        <UserNameForm>UserName</UserNameForm>
+        <LastSeenForm>last:seen</LastSeenForm>
+      </UserDisplayForm>
+      <img src={info} className="add" alt="add" />
+      <img src={search} className="add" alt="add" />
+    </TopBar>
+  )
 }
 
 Header.propTypes = {
-	UserName: PropTypes.string.isRequired,
-	lastSeen: PropTypes.string.isRequired,
-};
+  UserName: PropTypes.string.isRequired,
+  lastSeen: PropTypes.string.isRequired,
+}
 
-export default Header;
+export default Header
