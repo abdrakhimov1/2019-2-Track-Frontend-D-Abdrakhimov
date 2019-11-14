ab0dfeaf755c4fab382570885f81ddc6094c5b08
abdrakhimov1 <dan.abdrakhimov@yandex.ru>
2019-11-14 13:04:52 +0300

    different linters fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import styled from '@emotion/styled'
-import { keyframes } from '@emotion/core'
-import back from '../assets/back.png'
-import info from '../assets/info.png'
-import search from '../assets/search.png'
-import ava from '../assets/avatar.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/core';
+import back from '../assets/back.png';
+import info from '../assets/info.png';
+import search from '../assets/search.png';
+import ava from '../assets/avatar.png';
 
 const rotate360 = keyframes`
 	from {
@@ -15,7 +15,7 @@ const rotate360 = keyframes`
 	to {
 		transform: rotate(360deg);
 	}
-`
+`;
 
 const TopBar = styled.div`
   background-color: #550445;
@@ -30,7 +30,7 @@ const TopBar = styled.div`
     animation: ${rotate360} infinite 20s linear;
     height: 80px;
   }
-`
+`;
 
 const UserNameForm = styled.div`
   font-size: large;
@@ -38,39 +38,39 @@ const UserNameForm = styled.div`
   font-weight: bold;
   margin-left: 5px;
   text-align: left;
-`
+`;
 const LastSeenForm = styled.div`
   font-weight: 200;
   font-size: small;
   margin-right: 5px;
   text-align: left;
   margin-left: 5px;
-`
+`;
 const UserDisplayForm = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
 
 function Header({ UserName, lastSeen }) {
-  return (
-    <TopBar>
-      <Link to="/FriendList">
-        <img src={back} className="add" alt="add" />
-      </Link>
-      <img src={ava} className="ava" alt="ava" />
-      <UserDisplayForm>
-        <UserNameForm>UserName</UserNameForm>
-        <LastSeenForm>last:seen</LastSeenForm>
-      </UserDisplayForm>
-      <img src={info} className="add" alt="add" />
-      <img src={search} className="add" alt="add" />
-    </TopBar>
-  )
+	return (
+		<TopBar>
+			<Link to="/FriendList">
+				<img src={back} className="add" alt="add" />
+			</Link>
+			<img src={ava} className="ava" alt="ava" />
+			<UserDisplayForm>
+				<UserNameForm>UserName</UserNameForm>
+				<LastSeenForm>last:seen</LastSeenForm>
+			</UserDisplayForm>
+			<img src={info} className="add" alt="add" />
+			<img src={search} className="add" alt="add" />
+		</TopBar>
+	);
 }
 
 Header.propTypes = {
-  UserName: PropTypes.string.isRequired,
-  lastSeen: PropTypes.string.isRequired,
-}
+	UserName: PropTypes.string.isRequired,
+	lastSeen: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import styled from '@emotion/styled'
-import './style_css/Compose.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import './style_css/Compose.css';
 // import add from '../assets/add.png'
 // import pic from '../assets/pic.png'
 // import smile from '../assets/smile.png'
-import Message from './Message.js'
+import Message from './Message.js';
 
 const MessageListFull = styled.div`
   display: flex;
   flex-direction: column;
   overflow: auto;
   height: calc(100vh - 150px);
-`
+`;
 
 function MessageList({ messages }) {
-  return (
-    <MessageListFull>
-      {messages.map((item) => (
-        <Message key={item.id} {...item} />
-      ))}
-    </MessageListFull>
-  )
+	return (
+		<MessageListFull>
+			{messages.map((item) => (
+				<Message key={item.id} {...item} />
+			))}
+		</MessageListFull>
+	);
 }
 
 MessageList.propTypes = {
-  messages: PropTypes.isRequired,
-}
+	messages: PropTypes.isRequired,
+};
 
-export default MessageList
+export default MessageList;
